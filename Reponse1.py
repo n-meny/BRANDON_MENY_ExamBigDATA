8503bfc7da5735078510fa9f8ec8ae585641c742
Tastycoole <123739502+n-meny@users.noreply.github.com>
2025-05-06 13:53:10 +0200

    Update Reponse1.py

--- Reponse1.py
+++ Reponse1.py
@@ -6,16 +6,20 @@ class MovieTagCount(MRJob):
     def steps(self):
         return [
             MRStep(mapper=self.mapper_get_movie_tags,
-                reducer=self.reducer_count_tags)
+                   reducer=self.reducer_count_tags)
         ]
 
     def mapper_get_movie_tags(self, _, line):
-        if line.startswith("userId"):
-            return
-        fields = line.strip().split(',')
-        if len(fields) >= 3:
-            movieId = fields[1]
-            yield movieId, 1
+        try:
+            if line.startswith("userId"):
+                return
+            fields = line.strip().split(',')
+            if len(fields) >= 3:
+                movieId = fields[1]
+                yield movieId, 1
+        except Exception as e:
+            # Affiche l'erreur dans les logs de sortie standard
+            self.stderr.write(f"Erreur dans le mapper: {e}\n")
 
     def reducer_count_tags(self, movieId, counts):
         yield movieId, sum(counts)
